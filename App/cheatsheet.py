c59184d6c062d219dc2a6e77e179fd9f3e6287e4
Dushan <dushancimbaljevic@gmail.com>
2020-08-24 15:04:03 +0200

    cheetsheet done app pocnan

--- App/cheatsheet.py
+++ App/cheatsheet.py
@@ -34,6 +34,15 @@ NameLabel.pack()
 nameEn = tk.Entry(master=frameB,width=5)
 nameEn.pack()
 
+"""
+ Postojat pack , place , grid. Nie imame Frame(frameGrid) pripaga na window (master=window) u nego so .grid stavame drug frame(_frame)
+ u koj ima samo eden label(master=_frame) koj go dodaveme so .pack.Grid e samo frameGrid framovite u nego se .pack.Ovoa e praveno za
+ sekoja x,y kocka da ima svoja ramka
+ 
+        ||
+        \/
+  
+"""
 
 for x in range(3):
     for y in range(3):
@@ -43,8 +52,8 @@ for x in range(3):
         _label.pack()
 
 for i in range(3):
-    frameGrid.rowconfigure(i, weight=2 , minsize=50)
-    frameGrid.columnconfigure(i, weight=2 , minsize=50)
+    frameGrid.rowconfigure(i, weight=1 , minsize=50)
+    frameGrid.columnconfigure(i, weight=1 , minsize=50)
     
 frameB.pack(fill=tk.BOTH,expand=True)
 frame.pack(fill=tk.BOTH,expand=True)
