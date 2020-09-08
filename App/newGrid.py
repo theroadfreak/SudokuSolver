698c33a08dfc9a3dff1ff6d707c5f53b19f6c8a4
theroadfreak <dushancimbaljevic@gmail.com>
2020-09-08 15:06:36 -0700

    vozmoznosti dodadeni ama ne gotovo

--- App/newGrid.py
+++ App/newGrid.py
@@ -1,11 +1,14 @@
 import tkinter as tk
 
 
+
 window = tk.Tk()
 gridFrame = tk.Frame(master=window)
 
+
 commonSet1=(1,2,3)
 commonSet2=(4,5,6)
+entryList = list()
 
 
 largeFrameList = list()
@@ -18,7 +21,6 @@ for x in range(3): #sozdavam 9 golemi frame i skladiram u 2d lista largeFrameLis
     largeFrameList.append(tSetOfThree)
 
 
-entryList = list()
 largeframeX = int()
 largeframeY = int()
 gridX = int()
@@ -50,13 +52,34 @@ for fakeY in range(1,10):
         entry = tk.Entry(master=largeFrameList[largeframeY][largeframeX],width=4)
         entry.grid(row=gridY,column=gridX)
         
-        # label = tk.Label(master=largeFrameList[largeframeY][largeframeX],text="{},{}".format(fakeX,fakeY) ,width=4,height=2)
-        # label.grid(row=gridY,column=gridX)
+        # label = tk.Label(master=largeFrameList[largeframeY][largeframeX],text="{},{}".format(fakeX,fakeY) ,width=4,height=2)   #debuging
+        # label.grid(row=gridY,column=gridX)    #debuging
         
         tXentrys.append(entry)
-        # tXentrys.append(label)
+        # tXentrys.append(label) #debuging
     entryList.append(tXentrys)      #skladiram u 2d zada moze entryList[x][y]
 
+
+def possible(x,y):
+    
+    value = entryList[x][y].get()
+
+    for axisY in range(0,9):
+        if y != axisY:
+            if entryList[x][axisY].get() == value:
+                return False
+    
+    for axisX in range(0,9):
+        if x != axisX:
+            if entryList[axisX][y].get() == value:
+                return False
+    
+    return True
+    
+
+def Solve():
+    print(possible(0,0))
+SolveButton = tk.Button(master=window,text="Solve",width=36,font=("", 12), command=Solve)
 
 ErrorLabel = None
 def clickEvent(*args): #check if ima bukvi ili if >9
@@ -64,7 +87,7 @@ def clickEvent(*args): #check if ima bukvi ili if >9
     
     if ErrorLabel:
         ErrorLabel.destroy()
-        #SolveButton.config(state=tk.NORMAL)    
+        SolveButton.config(state=tk.NORMAL)    
         
     for row in entryList:
         for value in row:
@@ -78,12 +101,7 @@ def clickEvent(*args): #check if ima bukvi ili if >9
                 except:
                     ErrorLabel = tk.Label(master=window, text="You can only enter numbers! (less than 9)", fg="red")
                     ErrorLabel.pack()            
-                    #SolveButton.config(state=tk.DISABLED)
-
-
-SolveButton = tk.Button(master=window,text="Solve",width=36,font=("", 12), command=Solve)
-def Solve():
-    pass
+                    SolveButton.config(state=tk.DISABLED)
 
 
 window.bind("<Button-1>",clickEvent)
